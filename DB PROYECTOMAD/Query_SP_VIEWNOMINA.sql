da4c06c702b63adfcd17c240008fe146c1ca220f
pao_cuellar <pao.cuellar18@hotmail.com>
2022-05-22 09:23:39 -0500

    Stored procedure de Ver nomina de x empleado

---
+++ DB PROYECTOMAD/Query_SP_VIEWNOMINA.sql
@@ -0,0 +1,31 @@
+USE PROYECTOMAD
+
+IF EXISTS (SELECT 1 FROM sysobjects WHERE name = 'sp_ViewNomina' AND type = 'P')
+    DROP PROCEDURE sp_ViewNomina;
+GO
+
+CREATE PROCEDURE sp_ViewNomina(
+	@Fecha DATE = NULL,
+	@CveEmpleado INT = NULL
+)
+AS
+BEGIN
+	
+	SELECT	Nomina.ID_Nomina, 
+			Nomina.CveEmpleado 'Clave Empleado', 
+			Departamento.Nombre 'Departamento',
+			Departamento.Sueldo_Base 'Sueldo Base', 
+			Puesto.Nombre 'Puesto',
+			Puesto.Nivel_Salarial 'Nivel Salarial',
+			Nomina.Salario_Diario 'Salario Diario',
+			Nomina.Sueldo_Bruto 'Sueldo Bruto',
+			Nomina.Sueldo_Neto 'Sueldo Neto'
+			-- Ver como mostrar todas las percepciones de ese empleado
+			-- Ver como mostrar todas las deducciones de ese empleado
+	FROM Empleado
+	JOIN Nomina ON Nomina.CveEmpleado=Empleado.CveEmpleado
+	JOIN Departamento ON Departamento.ID_Departamento=Empleado.ID_Departamento
+	JOIN Puesto ON Puesto.ID_Puesto=Empleado.ID_Puesto
+	WHERE Nomina.CveEmpleado=@CveEmpleado AND Nomina.Fecha=@Fecha;
+
+END
